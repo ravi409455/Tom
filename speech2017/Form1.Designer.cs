e3b442432688a863fc99a6d6e99ddf393b690786
Ravi Kumar <ravi409455@gmail.com>
2018-06-25 00:11:54 +0530

    Added

---
+++ speech2017/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace speech2017
+{
+    partial class Tom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tom));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.textBox2.HideSelection = false;
+            this.textBox2.Location = new System.Drawing.Point(479, 150);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(209, 121);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.textBox1.Location = new System.Drawing.Point(12, 150);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(209, 121);
+            this.textBox1.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Gabriola", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(12, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 39);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Mode : Awake";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("OCR A Extended", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Purple;
+            this.label4.Location = new System.Drawing.Point(274, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "A Simple A.I.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(509, 349);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "By Ravi Kumar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Maiandra GD", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(265, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 77);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Tom";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::speech2017.Properties.Resources.img2;
+            this.pictureBox1.Location = new System.Drawing.Point(297, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM8";
+            // 
+            // Tom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::speech2017.Properties.Resources.img1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(700, 381);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Tom";
+            this.Text = "Tom";
+            this.Load += new System.EventHandler(this.Tom_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ speech2017/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+
+
+namespace speech2017
+{
+    public partial class Tom : Form
+    {
+        private readonly SpeechSynthesizer s = new SpeechSynthesizer();
+        private readonly SpeechRecognitionEngine reco = new SpeechRecognitionEngine();
+        private readonly Choices list = new Choices();
+        private Boolean mode = true;
+        private readonly string _name = "ravi";
+        public static string GreetRandom()
+        {
+            var greetings = new string[5] { "Hi sir", "Welcome back Ravi", "Nice to see you again Ravi", "What is the plan today Ravi ", "Good to see you again" };
+            Random r = new Random();
+            return greetings[r.Next(5)];
+
+        }
+        private static void Restart()
+        {
+            Process.Start(@"C:\Users\Ravi\Documents\Visual Studio 2017\Projects\speech2017\speech2017\bin\Debug\speech2017.exe");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public Tom()
+        {
+            InitializeComponent();
+            s.SelectVoiceByHints(VoiceGender.Male);
+            Sayit(GreetRandom());
+           list.Add(File.ReadAllLines(@"C:\Users\Ravi\Documents\Visual Studio 2017\Projects\speech2017\speech2017\commands.txt"));
+            Grammar gm = new Grammar(new GrammarBuilder(list));
+            //DictationGrammar gm = new DictationGrammar();
+            try
+            {
+                //serialPort1.Open();
+                reco.RequestRecognizerUpdate();
+                reco.LoadGrammar(gm);
+                reco.SpeechRecognized += Reco_SpeechRecognized;
+                reco.SetInputToDefaultAudioDevice();
+                reco.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch
+            {
+                MessageBox.Show(@"Arduino is not connected. Some features might not work properly.");
+            }
+
+
+        }
+
+        private void Sayit(string v)
+        {
+            s.SpeakAsync(v);
+            textBox2.AppendText(v + "\n");
+        }
+
+        private void Reco_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string a = e.Result.Text;
+            textBox1.AppendText(a + "\n");
+            
+            if (a == "sleep")
+            {
+                Sayit("OK sir");
+                label2.Text = @"Mode : Sleep";
+                mode = false;
+            }
+            if (a == "wake")
+            {
+                Sayit("Good Morning sir");
+                label2.Text = @"Mode : Awake";
+                mode = true;
+            }
+
+            //if (search == true)
+            //{
+            //    Process.Start("https://www.google.co.in/search?dcr=0&source=hp&ei=9Tg-Wo7bOoTsvgT55JjICw&q=" + a + "&oq=" + a + "&gs_l=psy-ab.3...2474.3785.0.3958.0.0.0.0.0.0.0.0..0.0....0...1c.1.64.psy-ab..0.0.0....0.4opvkPS0c_E");
+            //    search = false;
+            //}
+
+            if (mode == true )// && search == false)
+            {
+                // if (a == "change my name")
+                //{
+                //  sayit("What is your name");
+                //File.WriteAllText(@":\users\ravi\documents\visual studio 2015\Projects\speech\speech\name.txt", a);
+                //}
+                switch (a)
+                {
+                    case ("hi tom"):
+                    case ("hello tom"):
+                    case ("hello"):
+                        Sayit("Hello Ravi");
+                        break;
+
+                    //case ("search for"):
+                    //    search = true;
+                    //    break;
+
+                    case ("on kar"):
+                        if (!serialPort1.IsOpen)
+                        {
+                            serialPort1.Open();
+                        }
+                        serialPort1.Write("1");
+                        Sayit("Lights are turned On");
+                        break;
+
+                    case ("off kar"):
+                        if (serialPort1.IsOpen)
+                        {
+                            serialPort1.Write("0");
+                            serialPort1.Close();
+                        }
+                        Sayit("Lights are turned Off");
+                        break;
+
+                    case ("what is my name"):
+                        Sayit("Your name is " + _name);
+                        break;
+
+                    case ("who is your father"):
+                    case ("who created you"):
+                        Sayit("I am created by Mister Ravi Kumar");
+                        break;
+
+                    case ("hi"):
+                    case ("hi buddy"):
+                    case ("hey"):
+                        Sayit("Hey");
+                        break;
+
+                    case ("how are you tom"):
+                    case ("how are you"):
+                        Sayit("I am fine Sir, what about you");
+                        break;
+
+                    case ("who are you"):
+                    case ("what is your name"):
+                        Sayit("I am Tom, but there is no Jerry");
+                        break;
+
+                    case ("what do you do"):
+                        Sayit("You can operate your computer and household appliances using me, just by giving your voice commands. You just say and I will do the task for you.");
+                        break;
+
+                    case ("show me your code"):
+                        textBox2.AppendText("I am written using C# language. My code is this ");
+                        s.Speak("I am written using CSharp language. My code is this ");
+                        Process.Start(@"C:\Users\Ravi\Documents\Visual Studio 2017\Projects\speech2017\speech2017\tom's code.txt");
+                        break;
+
+                    case ("open folder"):
+                        Sayit("Opening it");
+                        Process.Start("explorer.exe");
+                        break;
+
+                    case ("open d drive"):
+                        Sayit("Opening it");
+                        Process.Start(@"D:\");
+                        break;
+
+                    case ("open google"):
+                        Sayit("Opening Google");
+                        Process.Start("https://www.google.co.in");
+                        break;
+
+                    case ("open calculator"):
+                        Sayit("Opening Calculator");
+                        Process.Start(@"D:\work-dev\Calci - Copy\index.html");
+                        break;
+
+                    case ("open notepad"):
+                        Sayit("Opening Note pad");
+                        Process.Start("notepad.exe");
+                        break;
+
+                    case ("open word"):
+                        Sayit("Cool");
+                        Process.Start(@"C:\Program Files\Microsoft Office\Office15\WINWORD.exe");
+                        break;
+
+                    case ("create a blank document"):
+                        SendKeys.Send("^(n)");
+                        break;
+
+                    case ("open powerpoint"):
+                        Sayit("Cool");
+                        Process.Start(@"C:\Program Files\Microsoft Office\Office15\POWERPNT.exe");
+                        break;
+
+                    case ("open excel"):
+                        Sayit("Cool");
+                        Process.Start("C:\\Program Files\\Microsoft Office\\Office15\\EXCEL.exe");
+                        break;
+
+                    case ("open music"):
+                        Sayit("Sure Sir");
+                        Process.Start(@"C:\Users\Ravi\Desktop\Groove Music.lnk");
+                        break;
+
+                    case ("gaane chala"):
+                        SendKeys.Send("^(p)");
+                        break;
+
+                    case ("play"):
+                        SendKeys.Send("^(p)");
+                        break;
+
+                    case ("ruk"):
+                        SendKeys.Send("^(p)");
+                        break;
+
+                    case ("pause"):
+                        SendKeys.Send("^(p)");
+                        break;
+
+                    case ("next"):
+                        SendKeys.Send("^(f)");
+                        break;
+
+                    case ("previous"):
+                        SendKeys.Send("^(b)");
+                        break;
+
+                    case ("bye"):
+                        textBox2.AppendText("Bye sir, Have a nice day");
+                        s.Speak("Bye sir, Have a nice day");
+                        
+                        Application.Exit();
+                        break;
+
+                    case ("hide yourself"):
+                        WindowState = FormWindowState.Minimized;
+                        break;
+
+                    case ("comeback"):
+                        WindowState = FormWindowState.Normal;
+                        break;
+
+                    case ("fullscreen"):
+                        WindowState = FormWindowState.Maximized;
+                        break;
+
+                    case ("close it"):
+                        SendKeys.Send("%{F4}");
+                        break;
+
+                    case ("stop talking"):
+                        s.SpeakAsyncCancelAll();
+                        break;
+
+                    //case ("open browser"):
+                    //    sayit("Opening browser");
+                    //    Form browser = new browser();
+                    //    WindowState = FormWindowState.Minimized;
+                    //    browser.Show();
+                    //    break;
+
+                    case ("do you love me"):
+                        Sayit("Yes sir, I love you so much");
+                        break;
+
+                    case ("what is today"):
+                        Sayit("Today is " + DateTime.Now.ToLongDateString());
+                        break;
+
+                    case ("what is the time"):
+                        Sayit("Time is " + DateTime.Now.ToString(@"hh:mm tt"));
+                        break;
+
+                    case ("lock computer"):
+                        Sayit("OK Sir");
+                        Process.Start(@"C:\WINDOWS\system32\rundll32.exe", "user32.dll,LockWorkStation");
+                        break;
+
+                    case ("restart"):
+                        textBox2.AppendText("Restarting");
+                        s.Speak("Restarting");
+                        Restart();  
+                        Application.Exit();
+                        break;
+                }
+            }
+        }
+
+        private void Tom_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
